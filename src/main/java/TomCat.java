302d077a4aabc89005bbb99c86163801ed74160c
Maria Shchurko <mashkap5@rambler.ru>
2022-12-24 14:37:16 +0300

    HelloWorld!

---
+++ src/main/java/TomCat.java
@@ -0,0 +1,19 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+    public class TomCat extends HttpServlet {
+
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<h2>Hello, World!</h2>");
+            out.close();
+        }
+    }
+
